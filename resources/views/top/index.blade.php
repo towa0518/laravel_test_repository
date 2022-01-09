693e7e15da8596e2161100ea94635bb721007fdb
towa0518 <sakasyu.10.22@outlook.com>
2022-01-09 22:15:15 +0900

    ファビコンを別ページに追加

---
+++ resources/views/top/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('images/favicon/favicon_pc.ico') }}">
+    <title>Top Page</title>
+</head>
+<body>
+    <div>Hello Laravel!</div>
+</body>
+</html>
